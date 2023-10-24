41e6af6725e7a31ce47c5c6f46c37fbb1b7835e2
Artur Damaceno Abreu <arturabreu110@gmail.com>
2023-10-24 17:31:14 -0300

    fix: tentando de novo

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -10,20 +10,12 @@ function getAbsolutePath(value: string): any {
   return dirname(require.resolve(join(value, "package.json")));
 }
 const config: StorybookConfig = {
-  stories: [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx|mdx)",
-    "../src/pages/**/*.stories.mdx",
-    "../src/stories/**/*.stories.tsx",
-    "../src/stories/**/*.stories.js",
-  ],
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
     getAbsolutePath("@storybook/addon-links"),
     getAbsolutePath("@storybook/addon-essentials"),
     getAbsolutePath("@storybook/addon-onboarding"),
     getAbsolutePath("@storybook/addon-interactions"),
-    "@storybook/addon-themes",
-    '@storybook/addon-a11y',
   ],
   framework: {
     name: getAbsolutePath("@storybook/react-vite"),
